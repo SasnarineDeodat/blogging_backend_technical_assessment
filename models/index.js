1f518d4fa46acca9a0e8fefcacae20923046cdc7
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-24 10:19:36 -0500

    added some more comments in the models index.js file for better code readability

--- models/index.js
+++ models/index.js
@@ -3,27 +3,31 @@ import userModel from "./user.model.js";
 import postModel from "./post.model.js";
 import commentModel from "./comment.model.js";
 
+// Initialize Sequelize connection to PostgreSQL database
 const sequelize = new Sequelize(
-  process.env.POSTGRESQL_DB,
-  process.env.POSTGRESQL_USER,
-  process.env.POSTGRESQL_PASSWORD,
+  process.env.POSTGRESQL_DB, // Database name
+  process.env.POSTGRESQL_USER, // Database user
+  process.env.POSTGRESQL_PASSWORD, // Database password
   {
-    host: process.env.POSTGRESQL_HOST,
-    port: process.env.POSTGRESQL_PORT,
-    dialect: process.env.POSTGRESQL_DIALECT,
-    logging: false,
+    host: process.env.POSTGRESQL_HOST, // Database host
+    port: process.env.POSTGRESQL_PORT, // Database port
+    dialect: process.env.POSTGRESQL_DIALECT, // Database dialect
+    logging: false, // Disable SQL logging for cleaner console output
 
+    // Connection pool settings for managing database connections
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
+      max: 5, // Maximum number of connections in pool
+      min: 0, // Minimum number of connections in pool
+      acquire: 30000, // Maximum time, in milliseconds, that pool will try to get connection before throwing error
+      idle: 10000, // Maximum time, in milliseconds, that a connection can be idle before being released
     },
   },
 );
 
+// Initialize database object to hold our models
 const db = {};
 
+// Attach Sequelize library and initialized instance to the database object
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
