e1b0c666b3b6d32d21d8a78b3dc80fa2aae890b0
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-24 10:20:37 -0500

    added some more comments in the user.model.js file for better code readability

--- models/user.model.js
+++ models/user.model.js
@@ -1,14 +1,20 @@
+// This model represents the users of the blogging platform, with fields for username, email, and password.
 const userModel = (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
+    // The username field: a string that must be provided (cannot be null) for each user.
     username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // The email field: a unique string that must be provided for each user.
+    // The 'unique' constraint ensures no two users can have the same email address.
     email: {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false,
     },
+    // The password field: a string that must be provided for each user.
+    // In practice, this should be a hashed password for security reasons.
     password: {
       type: DataTypes.STRING,
       allowNull: false,
