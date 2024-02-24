3544716084713218e5ef72729f941f1782820b90
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-24 10:36:01 -0500

    added a commit to describe the function in the isAuthenticated.js middleware

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,3 +1,15 @@
+/**
+ * Middleware to check if the user is authenticated.
+ *
+ * This middleware utilizes Passport's req.isAuthenticated() method to determine if the user
+ * has been authenticated. If the user is authenticated, it allows the request to proceed to the
+ * next middleware or route handler. If not, it responds with a 401 Unauthorized status and a
+ * message indicating that the user is not authenticated.
+ *
+ * @param {Object} req - The request object, which includes the isAuthenticated method from Passport.
+ * @param {Object} res - The response object, used to send a response if the user is not authenticated.
+ * @param {Function} next - The next middleware function in the stack.
+ */
 export const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next(); // User is authenticated, proceed to the next middleware or route handler
