69dd7c05581bdc4d9fd2fc42cc11042423f6273b
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-23 21:52:08 -0500

    setup .env file and setup dotenv module to get env variables. the details about how to setup .env will soon be made available on the readme file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
