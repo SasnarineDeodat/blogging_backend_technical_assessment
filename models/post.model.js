c16d8df19bbe14e2af1058f4fd4b4594d117c4ce
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-24 10:22:02 -0500

    added some comments in the post.model.js file for better code readability

--- models/post.model.js
+++ models/post.model.js
@@ -1,17 +1,20 @@
+// This model represents blog posts created by users, including their title, content, and publication status.
 const postModel = (sequelize, DataTypes) => {
   const Post = sequelize.define("post", {
+    // The title field: A string that tells what the blog post is about.
     title: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false, // Cannot be null
     },
+    // The title field: It contains the main body of the blog post.
     content: {
       type: DataTypes.TEXT,
-      allowNull: false,
+      allowNull: false, // Cannot be null
     },
     // Assuming you want to keep track of the published status
     published: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false,
+      defaultValue: false, // Published status will be false by default
     },
   });
 
