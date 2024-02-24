3694b9245ebd552b42f2d8d36f09f84d3f6e7160
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-24 10:23:08 -0500

    added some comments in the comment.model.js file for better code readability

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,12 +1,15 @@
+// This model represents comments made by users on blog posts, capturing the comment content and its association with a specific post.
 const commentModel = (sequelize, DataTypes) => {
   const Comment = sequelize.define("comment", {
+    // The content field: a text field that stores the body of the comment.
     content: {
       type: DataTypes.TEXT,
-      allowNull: false,
+      allowNull: false, // Cannot be null
     },
+    // The postId field: an integer that references the ID of the post to which this comment belongs.
     postId: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: false, // Cannot be null
     },
   });
 
