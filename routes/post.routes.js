a0c8a92cd664a6f40e8f984e79f6ef2b8321f445
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-24 10:24:28 -0500

    added some comments in the post.routes.js file for better code readability

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -10,11 +10,19 @@ import {
 const postRouter = (app) => {
   const router = express.Router();
 
+  // Route to create a new post. Requires user authentication.
   router.post("/", isAuthenticated, createPost);
+
+  // Route to retrieve posts by a user's email. This route does not require authentication.
   router.get("/by-email/:email", findPostsByEmail);
+
+  // Route to update an existing post. Requires user authentication and that the user owns the post.
   router.put("/:id", isAuthenticated, updatePost);
+
+  // Route to delete an existing post. Requires user authentication and that the user owns the post.
   router.delete("/:id", isAuthenticated, deletePost);
 
+  // Register the router with the application under the '/api/posts' path.
   app.use("/api/posts", router);
 };
 
