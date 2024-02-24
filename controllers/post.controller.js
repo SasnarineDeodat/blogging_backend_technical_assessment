3e4d409977b982bf89db624c2711841ea0e5bed0
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-24 09:46:03 -0500

    added the function to find all posts by a user using his email

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -18,3 +18,29 @@ export const createPost = async (req, res) => {
     });
   }
 };
+
+// Find all posts by a user's email
+export const findPostsByEmail = async (req, res) => {
+  const email = req.params.email;
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).send({ message: "User not found." });
+    }
+    const posts = await Post.findAll({
+      where: { userId: user.id },
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["id", "username", "email"],
+        },
+      ],
+    });
+    res.json(posts);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving posts.",
+    });
+  }
+};
